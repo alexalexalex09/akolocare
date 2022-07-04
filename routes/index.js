66cdb29abd596a87fec9cde6bb9b6b631aff918d
Alex <frogitts@gmail.com>
2022-07-04 08:46:16 -0400

    tos update

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get("/dashboard", function (req, res, next) {
   res.render("dashboard", { title: "AkoloCare Dashboard" });
 });
 
+/* GET privacy/tos. */
+router.get("/privacy-tos", function (req, res, next) {
+  res.render("privacy-tos", { title: "AkoloCare Dashboard" });
+});
+
 module.exports = router;
